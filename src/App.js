b678b45497e35e23988542a12caefdc61827f6ae
Nick Nedev <nedevn@gmail.com>
2018-03-15 22:06:48 -0700

    Creates components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Import Components
+import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h2>Working</h2>
+        <Header></Header>
         <Navigation></Navigation>
+        <Footer></Footer>
       </div>
     );
   }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
-class Header extends Component{
+class Footer extends Component{
   render(){
     return (
       <div>
-        <p> Header Rendered </p>
+        <p> Footer Rendered </p>
       </div>
     )
   }
 }
 
-export default Header;
+export default Footer;
